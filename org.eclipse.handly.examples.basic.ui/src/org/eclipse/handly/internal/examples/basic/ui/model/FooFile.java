b5dadad844af4b4fb727293e0f4d8a120fbeece3
Vladimir Piskarev <pisv@1c.ru>
2017-06-16 20:41:26 +0300

    Migrate to Handly 0.7

--- org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/internal/examples/basic/ui/model/FooFile.java
+++ org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/internal/examples/basic/ui/model/FooFile.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2016 1C-Soft LLC and others.
+ * Copyright (c) 2014, 2017 1C-Soft LLC and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -85,8 +85,8 @@ public class FooFile
     }
 
     @Override
-    protected void hBuildStructure(IContext context, IProgressMonitor monitor)
-        throws CoreException
+	public void hBuildSourceStructure(IContext context,
+	    IProgressMonitor monitor) throws CoreException
     {
         Map<IElement, Object> newElements = context.get(NEW_ELEMENTS);
         SourceElementBody body = new SourceElementBody();
